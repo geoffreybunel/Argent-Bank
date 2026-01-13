d3d3a594c987e469340f6437d5af7dfe9c5be1f4
Geoffrey Bunel <geoffreybunel1999@gmail.com>
2026-01-13 12:34:43 +0100

    Edit name

--- src/components/UserHero.jsx
+++ src/components/UserHero.jsx
@@ -1,8 +1,75 @@
+import { useState } from "react"
+import { useSelector, useDispatch } from "react-redux";
+import axiosInstance from '../api/axiosInstance';
+import { setUser } from '../redux/authSlice';
+
 function UserHero({ username, buttonText }) {
+    const dispatch = useDispatch();
+    const token = useSelector((state) => state.auth.token);
+    const user = useSelector((state) => state.auth.user);
+
+    const [isEditing, setIsEditing] = useState(false);
+    const [firstName, setFirstName] = useState(user.firstName);
+    const [lastName, setLastName] = useState(user.lastName);
+    const [loading, setLoading] = useState(false);
+
+    const handleSave = async () => {
+        if (!firstName || !lastName) return;
+    
+        setLoading(true);
+    
+        try {
+          const response = await axiosInstance.put(
+            '/profile',
+            { firstName, lastName },
+            {
+              headers: {
+                Authorization: `Bearer ${token}`,
+              },
+            }
+          );
+    
+          dispatch(setUser(response.data.body));
+          setIsEditing(false);
+        } catch {
+          console.error('Erreur de mise à jour du profil');
+        } finally {
+          setLoading(false);
+        }
+      };
+
     return (
         <div className="header">
-            <h1>Welcome back<br />{username}!</h1>
-            <button className="edit-button">{buttonText}</button>
+            {!isEditing ? (
+                <>
+                    <h1>Welcome back<br />{username}!</h1>
+                    <button className="edit-button" onClick={() => setIsEditing(true)}>{buttonText}</button>
+                </>
+            ) : (
+                <>
+                    <h1>Welcome back</h1>
+                    <div className="edit-name-form">
+                        <input
+                            type="text"
+                            value={firstName}
+                            onChange={(e) => setFirstName(e.target.value)}
+                        />
+                        <input
+                            type="text"
+                            value={lastName}
+                            onChange={(e) => setLastName(e.target.value)}
+                        />
+                        <div className="buttons">
+                            <button  className="edit-button" onClick={handleSave} disabled={loading}>
+                                Save
+                            </button>
+                            <button className="edit-button cancel" onClick={() => setIsEditing(false)}>
+                                Cancel
+                            </button>
+                        </div>
+                    </div>
+                </>
+            )}
         </div>
     )
 }
