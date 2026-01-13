351c2289d601b203612be60401250b4ec33d76b6
Geoffrey Bunel <geoffreybunel1999@gmail.com>
2026-01-13 12:36:01 +0100

    minor changes

--- src/pages/User.jsx
+++ src/pages/User.jsx
@@ -17,19 +17,19 @@ function User() {
             <h2 className="sr-only">Accounts</h2>
             <Account 
                 title="Argent Bank Checking (x8349)"
-                amount="$2,082.79"
+                amount="$2,000.00"
                 description="Available Balance"
                 buttonText="View transactions"
             />
             <Account 
                 title="Argent Bank Savings (x6712)"
-                amount="$10,928.42"
+                amount="$10,000.00"
                 description="Available Balance"
                 buttonText="View transactions"
             />
             <Account 
                 title="Argent Bank Credit Card (x8349)"
-                amount="$184.30"
+                amount="$100.00"
                 description="Current Balance"
                 buttonText="View transactions"
             />
